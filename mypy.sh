64431f25851e5e9d79700a7872b4f50f98ee2a4f
Nicolas Lara <nicolaslara@gmail.com>
2017-06-13 16:31:54 +0200

    only delete the generated file when there are no type errors

--- mypy.sh
+++ mypy.sh
@@ -3,6 +3,6 @@ if jupyter nbconvert --to script $1; then
 	echo 'Checking types with MyPy'
 	if mypy `basename $1 .ipynb`.py; then
 		echo 'No type errors found'
+		rm `basename $1 .ipynb`.py
 	fi
-	rm `basename $1 .ipynb`.py
 fi
